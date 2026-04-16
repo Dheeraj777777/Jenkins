e64c93251f166f652bdc6b230bfc407ff46981f4
Dheeraj777777 <Obito135710@gmail.com>
2026-04-16 09:29:47 +0530

    Update App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,23 +1,16 @@
 package com.example;
 
-import java.io.IOException;
-import java.io.OutputStream;
-import java.net.InetSocketAddress;
-import com.sun.net.httpserver.HttpServer;
-
 public class App {
-    public static void main(String[] args) throws IOException {
-        HttpServer server = HttpServer.create(new InetSocketAddress(3000), 0);
+    public static String checkNumber(int num) {
+        if (num % 2 == 0) {
+            return "Even";
+        } else {
+            return "Odd";
+        }
+    }
 
-        server.createContext("/", (exchange) -> {
-            String response = "App is running on port 3000";
-            exchange.sendResponseHeaders(200, response.length());
-            OutputStream os = exchange.getResponseBody();
-            os.write(response.getBytes());
-            os.close();
-        });
-
-        server.start();
-        System.out.println("Server started at http://localhost:3000");
+    public static void main(String[] args) {
+        int number = 5;
+        System.out.println(number + " is " + checkNumber(number));
     }
 }
